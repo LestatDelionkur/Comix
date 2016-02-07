10ce3398e9c65cb1a9d6cd289b65d19b5f0fc69d
Lestat Delionkur <lestatms@outlook.com>
2016-02-07 12:40:06 +0300

    Вторая миграция

--- src/Comix/Migrations/20160207093914_SecondMigration.Designer.cs
+++ src/Comix/Migrations/20160207093914_SecondMigration.Designer.cs
@@ -8,9 +8,10 @@ using Comix.Models.Base;
 namespace Comix.Migrations
 {
     [DbContext(typeof(ComixDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160207093914_SecondMigration")]
+    partial class SecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
